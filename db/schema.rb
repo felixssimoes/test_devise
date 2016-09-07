bda42f7343742cefac9fc1c6533cd348dc76b57c
Felix Simoes <felix.simoes@njiuko.com>
2016-09-07 15:25:58 +0200

    Add name to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160907115528) do
+ActiveRecord::Schema.define(version: 20160907123139) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160907115528) do
     t.integer  "invited_by_id"
     t.string   "invited_by_type"
     t.integer  "invitations_count",      default: 0
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
