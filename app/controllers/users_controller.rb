cc36ce2de282bdf86f399d4536c8f10ae62284c7
Felix Simoes <felix.simoes@njiuko.com>
2016-09-08 18:56:31 +0200

    Improve users controller with more standard rails

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,29 @@
 class UsersController < ApplicationController
+  respond_to :html
+
+  before_filter :load_user, only: [:edit, :update]
+
   def index
     @users = User.all
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to users_path, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update(user_params)
+      flash[:success] = 'User was successfully updated.'
     end
+    respond_with @user, location: users_path
   end
 
   private
 
   def user_params
     params.require(:user).permit(:name)
+  end
+
+  def load_user
+    @user = User.find(params[:id])
   end
 end
